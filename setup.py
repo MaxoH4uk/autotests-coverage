8cf0f8fbb8b78c8ad2e599034714a3c768342740
TikhomirovMS <maxim.tikhomirov@idp.zyfra.com>
2025-02-11 08:38:22 +0300

    upgrade package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="swagger-coverage-metrics",
-    version="0.0.1",
+    version="0.0.2-alpha.1",
     author="Jamal Zeinalov",
     author_email="jamal.zeynalov@gmail.com",
     description='Python adapter for "swagger-coverage" tool',
