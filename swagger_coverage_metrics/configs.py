3f99a05dc404dfecd85abc48a49546f12370c800
TikhomirovMS <maxim.tikhomirov@idp.zyfra.com>
2025-02-11 11:09:58 +0300

    add variables checker

--- swagger_coverage_metrics/configs.py
+++ swagger_coverage_metrics/configs.py
@@ -15,5 +15,11 @@ API_DOCS_TYPE = env.str("API_DOCS_TYPE", default="openapi")  # "swagger"
 API_DOCS_VERSION = env.str("API_DOCS_VERSION", default="3.0.0")  # "2.0"
 API_DOCS_FORMAT = env.str("API_DOCS_FORMAT", default="json")  # "yaml"
 DEBUG_MODE = env.bool("DEBUG_MODE", default=False)  # Set to True to see commandline output
-REPORTS_DIR = env.str("REPORTS_DIR", default="./")
-CONFIGS_DIR = env.str("CONFIGS_DIR", default=Path("./"))
+REPORTS_DIR = env.str("REPORTS_DIR", default=None)
+CONFIGS_DIR = env.str("CONFIGS_DIR", default=None)
+
+if REPORTS_DIR is None:
+    raise ValueError(f"Укажите значение переменной {REPORTS_DIR}.")
+
+if CONFIGS_DIR is None:
+    raise ValueError(f"Укажите значение переменной {CONFIGS_DIR}.")
