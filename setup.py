84bbbfb563dadd334a19f98cde68db2451a220f8
MaxoH4uk <2893_maxon41k@mail.ru>
2025-04-25 14:37:27 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="autotests-coverage",
-    version="1.0.0",
+    version="1.1.0",
     author="Jamal Zeinalov",
     author_email="jamal.zeynalov@gmail.com",
     description='Python adapter for "swagger-coverage" tool',
